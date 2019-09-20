033fc000f189ec020e855fc8fe900a655443676f
github username <github email address>
2019-09-19 19:44:48 -0700

    fixed planet filter function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,51 @@ import './App.css';
 // import universe from './pictures/universe.jpg'
 import PlanetHolder from './containers/PlanetHolder.js'
 
+const planetAPI = "https://api.le-systeme-solaire.net/rest/bodies/"
 
-function App() {
+const filteredPlanets = "haha"
+
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      planets: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(planetAPI)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.bodies)
+      this.setState({
+        planets: data.bodies
+      })
+    })
+  }
+
+  filterPlanets = () => {
+    var planets = this.state.planets
+    const filteredPlanets = planets.filter(planet => {
+        if(planet.isPlanet === true) {
+          return planet
+        }
+      }
+      )
+    console.log(filteredPlanets);
+
+  }
+
+  render(){
   return (
     <div className="App">
     <h1 className="header">Welcome to the Universe</h1>
-
+    {this.filterPlanets()}
     <PlanetHolder/>
     </div>
   );
+}
 }
 
 export default App;
